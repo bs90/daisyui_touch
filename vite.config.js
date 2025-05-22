a559bf5c990f3a84dcce3097f8b38552494ef411
TRONGTranBa <trongtb90@gmail.com>
2025-05-22 21:58:56 +0900

    g9

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
         index: resolve(__dirname, 'index.html'),
         signup: resolve(__dirname, 'signup.html'),
         login: resolve(__dirname, 'login.html'),
-        chatshow: resolve(__dirname, 'chatshow.html')
+        chatshow: resolve(__dirname, 'chatshow.html'),
+        chatlist: resolve(__dirname, 'chatlist.html')
       }
     }
   }
